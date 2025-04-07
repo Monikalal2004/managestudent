7eedb46885fe465982c748496d0065de291d7f1e
unknown <Monikalal>
2025-04-07 12:06:01 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentdetailsComponent } from './studentdetails/studentdetails.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,StudentdetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
